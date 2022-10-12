6b5658cdfde7b119788b5be9ba3f86428a67bdfe
fredemandensGit <frederik_hartmann@hotmail.com>
2022-10-12 11:30:04 +0200

    Added wandb

--- models/IntuBio_bacteria/create_enviroment.sh
+++ models/IntuBio_bacteria/create_enviroment.sh
@@ -5,19 +5,15 @@
 module load python3/3.10.2
 
 #Create a virtual environment for Python3
-python3 -m venv /zhome/2d/0/127322/endpoint_segmentation
+python3 -m venv /zhome/7e/4/126929/endpoint_segmentation
 
 #Activate virtual environment
 source ~/endpoint_segmentation/bin/activate
 
 #If pip3 fails, use: which pip3, to make sure it is the one in the virutal environment.
 #which pip3
-python3 -m pip install pip==21.3.1
-python3 -V
 
 python3 -m pip install hydra-core --upgrade
 python3 -m pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116
 python3 -m pip install -r requirements.txt
-
-
-
+python3 -m pip install -U git+https://github.com/albumentations-team/albumentations
