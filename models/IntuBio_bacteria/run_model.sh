5222353519b42ec5be7fffb424e98477dead7070
fredemandensGit <frederik_hartmann@hotmail.com>
2022-10-17 14:21:01 +0200

    altered basic and runmodel

--- models/IntuBio_bacteria/run_model.sh
+++ models/IntuBio_bacteria/run_model.sh
@@ -3,9 +3,9 @@
 #BSUB -o endpoint_%J.out
 #BSUB -e endpoint_%J.err
 #BSUB -q gpua100
-#BSUB -R "rusage[mem=8GB]"
+#BSUB -R "rusage[mem=32GB]"
 ## NUMBER OF CORES
-#BSUB -n 8 #IF USING ALL THREADS POSSIBLE
+#BSUB -n 1 #IF USING ALL THREADS POSSIBLE
 #BSUB -W 22:00
 #BSUB -N
 #BSUB -gpu "num=1"
@@ -14,7 +14,7 @@ load module python3/10.3.2
 load module cuda
 source ~/endpoint_segmentation/bin/activate
 # Get api key for wandb
-export WANDB_API_KEY=´cat wandbkey.txt´
+export WANDB_API_KEY=$(cat wandbkey.txt)
 wandb login #wandb login
 
-python3 ./src/models/train_model.py
+python3 src/models/train_model.py
