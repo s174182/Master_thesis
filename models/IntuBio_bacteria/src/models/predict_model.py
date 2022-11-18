1db4713e2d2014debd942d48a4719e3a5abdb55c
Jonathan <s174182@student.dtu.dk>
2022-11-18 11:14:44 +0100

    updated predict_model.py

--- models/IntuBio_bacteria/src/models/predict_model.py
+++ models/IntuBio_bacteria/src/models/predict_model.py
@@ -10,51 +10,6 @@ from patchify import patchify, unpatchify
 from scipy import ndimage
 
 
-def normimg(img,imgref):
-    IthrMask=18
-    imnorm=(170*(img-10).astype(float)/(imgref-10).astype(float))
-    imnorm[imnorm>255]=255
-    imnorm=imnorm.astype(np.uint8)
-
-    
-    th, im_th = cv2.threshold(imgref, IthrMask, 255, cv2.THRESH_BINARY);
-    im_th=ndimage.binary_fill_holes(im_th,structure =None,output =None,origin=0).astype(np.uint8)
-    
-    kernel = np.ones((25, 25), np.uint8)
-      
-    # # Using cv2.erode() method 
-    ROI = cv2.erode(im_th, kernel)
-
-    imnorm[ROI==0]=0
-    return imnorm 
-def ROI(mask,img):
-    IthrMask=18
-    th, im_th = cv2.threshold(img, IthrMask, 255, cv2.THRESH_BINARY);
-    im_th=ndimage.binary_fill_holes(im_th,structure =None,output =None,origin=0).astype(np.uint8)
-    
-    kernel = np.ones((30, 30), np.uint8)
-      
-    # # Using cv2.erode() method 
-    ROI = cv2.erode(im_th, kernel)
-
-    mask[ROI==0]=0
-    analysis=cv2.connectedComponentsWithStats(mask,4,cv2.CV_32S)
-    (totalLabels,label_ids,values,centroid)=analysis
-    output=np.zeros(mask.shape,dtype="uint8")
-    
-    for i in range(1,totalLabels):
-        area=values[i, cv2.CC_STAT_AREA]
-        
-        if area>150:
-            componentMask = (label_ids==i).astype("uint8")
-            output=cv2.bitwise_or(output,componentMask)
-            
-    return output 
-
-
-
-
-
 # Define a transform to convert the image to tensor
 transform = transforms.ToTensor()
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
